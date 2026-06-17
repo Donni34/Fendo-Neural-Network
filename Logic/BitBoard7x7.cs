e522d3533cd18da301ec54db2a26fd2f1a7af5c8
Leonard Wanjetscheck <go68xih@tum.de>
2026-06-17 23:14:11 +0200

    Bitboard

--- Logic/BitBoard7x7.cs
+++ Logic/BitBoard7x7.cs
@@ -1,14 +1,14 @@
-﻿namespace Fendo.Logic;
+﻿using System.Diagnostics.CodeAnalysis;
 
-public static class Default
+namespace Fendo.Logic;
+
+public static class BoardMasks
 {
-    public const ulong player1 = 0;
-    public const ulong player2 = 0;
-    public const ulong player1_sealed = 0;
-    public const ulong player2_sealed = 0;
-
-    public const ulong vertical_borders = ;
-    public const ulong horizontal_borders = ;
+    public const ulong ValidArea = 0x007F_7F7F_7F7F_7F7F;
+    public const ulong LeftBorder = 0x0001_0101_0101_0101;
+    public const ulong RightBorder = 0x0080_8080_8080_8080;
+    public const ulong TopBorder = 0x0000_0000_0000_007F;
+    public const ulong BottomBorder = 0x7F00_0000_0000_0000;
 }
 
 public class BitBoard7x7
@@ -17,31 +17,75 @@ public class BitBoard7x7
     public ulong horizontal_borders { get; private set; }
     public ulong player1 { get; private set; }
     public ulong player2 { get; private set; }
-    public ulong player1_sealed { get; private set; }
-    public ulong player2_sealed { get; private set; }
 
-    public byte[] pieces { get; private set; } = new byte[2];
-    public Player active_player { get; private set; } = Player.One;
+    public ulong AllPieces => player1 | player2;
 
-    private int? _hash = null;
-
-    public BitBoard7x7(ulong? player1 = null, ulong? player2 = null, ulong? player1_sealed = null, ulong? player2_sealed = null, ulong? horizontal_borders = null, ulong? vertical_borders = null)
+    public BitBoard7x7(ulong p1 = 0, ulong p2 = 0, ulong hBorders = 0, ulong vBorders = 0)
     {
-        this.player1 = player1 is ulong ? (ulong)player1 : Default.player1;
-        this.player2 = player2 is ulong ? (ulong)player2 : Default.player2;
-        this.player1_sealed = player1_sealed is ulong ? (ulong)player1_sealed : Default.player1_sealed;
-        this.player2_sealed = player2_sealed is ulong ? (ulong)player2_sealed : Default.player2_sealed;
-        this.horizontal_borders = (horizontal_borders is ulong ? (ulong)horizontal_borders : 0) | Default.horizontal_borders;
-        this.vertical_borders = (vertical_borders is ulong ? (ulong)vertical_borders : 0) | Default.vertical_borders;
+        player1 = p1;
+        player2 = p2;
+        horizontal_borders = hBorders;
+        vertical_borders = vBorders;
     }
 
-    public List<ulong> GetTurns()
-    {
+    #region Basics
+    public static bool HasBit(ulong board, int x, int y)
+        => (board & (1UL << (y * 8 + x))) != 0;
 
+    public static ulong SetBit(ulong board, int x, int y)
+        => board | (1UL << (y * 8 + x));
+
+    public ulong ShiftRight(ulong pieces) => (pieces << 1) & ~vertical_borders;
+    public ulong ShiftLeft(ulong pieces) => (pieces & ~vertical_borders) >> 1;
+    public ulong ShiftDown(ulong pieces) => (pieces << 8) & ~horizontal_borders;
+    public ulong ShiftUp(ulong pieces) => (pieces & ~horizontal_borders) >> 8;
+
+    public static ulong ShiftRight(ulong pieces, ulong borders) => (pieces << 1) & ~borders;
+    public static ulong ShiftLeft(ulong pieces, ulong borders) => (pieces & ~borders) >> 1;
+    public static ulong ShiftDown(ulong pieces, ulong borders) => (pieces << 8) & ~borders;
+    public static ulong ShiftUp(ulong pieces, ulong borders) => (pieces & ~borders) >> 8;
+    #endregion 
+
+    #region Vision
+    private ulong GetVisionTo(ulong origin, ulong obstructions, ulong borders, Border b)
+    {
+        ulong vision = 0;
+        ulong ray = origin;
+        for (int i = 0; i < 6; i++)
+        {
+            ray = b switch
+            {
+                Border.North => ShiftUp(ray, borders),
+                Border.South => ShiftDown(ray, borders),
+                Border.West => ShiftLeft(ray, borders),
+                Border.East => ShiftRight(ray, borders),
+                _ => 0,
+            };
+            if (ray == 0) break;
+            vision |= ray;
+            ray &= ~obstructions;
+        }
+        return vision;
     }
 
-    public ulong ObstructedVision(ulong vision, ulong obstruction_board, ulong h_borders, ulong v_borders)
+    public ulong GetVision(ulong origin, ulong obstructions, ulong hborders, ulong vborders, int depth = 2)
     {
-
+        if (depth <= 0) return origin;
+        ulong horizontal_vision = GetVisionTo(origin, obstructions, vborders, Border.East) | GetVisionTo(origin, obstructions, vborders, Border.West);
+        ulong vertical_vision = GetVisionTo(origin, obstructions, hborders, Border.North) | GetVisionTo(origin, obstructions, hborders, Border.South);
+        ulong vision = horizontal_vision | vertical_vision;
+        return vision == origin ? vision : GetVision(vision, obstructions, hborders, vborders, depth - 1);
     }
+    #endregion
+
+    #region Regions
+    public ulong GetRegion(ulong origin, ulong obstructions, ulong hborders, ulong vborders)
+    {
+        return GetVision(origin, obstructions, hborders, vborders, 49);
+    }
+    #endregion
+
+    #region Turns
+
+    #endregion
 }
